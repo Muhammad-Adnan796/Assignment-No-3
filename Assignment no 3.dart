997b5b5defe8c533a61839b58419b6d0513ea9b8
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-25 09:05:59 -0700

    Add files via upload

---
+++ Assignment no 3.dart
@@ -0,0 +1,65 @@
+void main() {
+//   Answer No 02
+  
+// List<int> lst1 = [1,2,3,4,5,6,7];
+// List<int> lst2 = [3,5,6,7,9,10];
+// List<int> difference = lst1.toSet().difference(lst2.toSet()).toList();
+// print(difference.toString());
+  
+
+//    Answer No 03
+  
+//     var num = 12; 
+//    if (num % 2==0) { 
+//       print("Even"); 
+//    } else { 
+//       print("Odd"); 
+//    } 
+  
+//   Answer No 05
+  
+//   var a1 = [7];
+//  for(var i=1; i<=15; i++){
+//     print ( '${7}* ${i} = ${7*i}');
+//   }
+  
+//   Answer No 06
+  
+//   var fruits = ['apple','banana','mango','orange','strawberry','date'];
+//   for( var i=0; i<=fruits.length-2; i++){
+//     print(fruits[i]);
+//   }
+  
+//   Answer No 07
+  
+//   var a1 = [5];
+//   for(var i=1; i<=100; i++){
+//     print (" ${5*i}");
+//   }
+  
+//   Answer No 08
+  
+//   var celcius1 = 12.5;
+//   print (' Celcius = ${celcius1} C');
+  
+//   var faranheit=(celcius1*9/5)+32;
+//   print(" Fahrenheit = ${faranheit} F  ");
+  
+//    var fahrenheit = 36.6;
+//   print ( ' Fahrenheit = ${fahrenheit} F ');
+//   var celcius = (fahrenheit-32)*5~/9 ;
+//   print (' Celcius = ${celcius} C');
+  
+
+  
+//  Answer No 10
+
+// String vowels = "i,e";
+//   if(vowels=='i,e'){
+//     print ('True');
+//   }else{
+//     print('false');
+//   }
+
+
+  }
